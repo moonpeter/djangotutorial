a63101ea3ec86494d6b61f588da8851fb71f0f4b
moonpeter <moonpeter@kakao.com>
2019-12-17 17:41:33 +0900

    index에서 latext_question_list를 보여줌

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
+from .models import Question
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question {question_id}.")
 
+
 def results(requst, question_id):
     response = "You're looking at the results of question {}"
     return HttpResponse(response.format(question_id))
+
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}.")
