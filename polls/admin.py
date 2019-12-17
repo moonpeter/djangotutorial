c27c4b09dc8ded5bfc441e1e10fe8da215718040
moonpeter <moonpeter@kakao.com>
2019-12-17 17:09:56 +0900

    LANGUAGE_CODE 및 TIEM_ZONE설정

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Question, Choice
+
+
 # Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
